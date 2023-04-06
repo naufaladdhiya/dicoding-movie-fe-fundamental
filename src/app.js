4c80aaa117f3a28e62599c8a6efc2a18d797b698
naufaladdhiya <naufal.addhiya@gmail>
2023-04-07 04:12:25 +0700

    add data in searchbar component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
-import "./style/style.css";
+import './style/style.css';
+import './script/components/navbar.js';
+import './script/components/heroes.js';
+import './script/components/searchbar.js';
+import './script/components/movie-list.js';
+import './script/components/footer.js';
+import main from './script/views/main.js';
+import 'regenerator-runtime';
 
-import main from "./script/views/main.js";
-import "./script/components/navigation-bar.js";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main);
