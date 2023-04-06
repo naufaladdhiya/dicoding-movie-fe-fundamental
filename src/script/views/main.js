e7d33e8b6593a6dc80c270171648bf199f4cc8a9
naufaladdhiya <naufal.addhiya@gmail>
2023-04-07 04:13:39 +0700

    add main to component

--- src/script/views/main.js
+++ src/script/views/main.js
@@ -1,3 +1,33 @@
-const main = () => {};
+import '../components/movie-list.js';
+import DataSource from '../data/data-source.js';
+
+const main = () => {
+  const movieListElement = document.querySelector('movie-list');
+  const buttonSearch = document.getElementById('searchButtonElement');
+  const valueSearch = document.getElementById('searchFilm');
+  const resultSearch = document.getElementById('hasilPencarian');
+
+  
+
+  const onButtonSearchClicked = async () => {
+    try {
+      const result = await DataSource.searchMovie(valueSearch.value);
+      renderResult(result);
+      resultSearch.innerText = `Menampilkan ${result.length} hasil dari ${valueSearch.value}`;
+    } catch (message) {
+      fallbackResult(message);
+    }
+  };
+
+  const renderResult = (results) => {
+    movieListElement.movies = results;
+  };
+
+  const fallbackResult = (message) => {
+    movieListElement.renderError(message);
+  };
+
+  buttonSearch.addEventListener('click', onButtonSearchClicked);
+};
 
 export default main;
